1ac6f56e7cb526578adbe5e1f3487d711a0efb09
tunde <fadipe11@gmail.com>
2022-07-23 13:03:08 +0100

    secret key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,7 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    secret: 'grace',
     callbacks: {
         async jwt({ token, account }) {
             // Persist the OAuth access_token to the token right after signin
